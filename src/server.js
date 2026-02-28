81e68b3892f61eb1678c2b00a34db95607f2774f
Lucas Sotero <lucas.abreu2@fit-tecnologia.org.br>
2026-02-28 10:49:00 -0400

    configurando inicial aplicação

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
-console.log('olá mundo')
+import http from "http";
+import { json } from "./middlewares/json.js";
+
+const server = http.createServer(async (req, res) => {
+    await json(req, res)
+})
+
+server.listen(3333)
