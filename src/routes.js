b6aaa32b58a8a39f06222ee2b28b1bb956e6f146
Lucas Sotero <lucas.abreu2@fit-tecnologia.org.br>
2026-02-28 12:01:08 -0400

    adicionando rota de post task

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import { randomUUID } from "crypto"
+import { buildRoutePath } from "./utils/build-route-path.js"
+import { Database } from "./database.js"
+
+const database = new Database()
+
+export const routes = [
+    {
+        method: 'POST',
+        path: buildRoutePath('/task'),
+        handler: (req, res) => {
+            const {
+                title,
+                description,
+            } = req.body
+
+            const task = {
+                id: randomUUID(),
+                title,
+                description,
+                created_at: new Date().toLocaleDateString('pt-BR'),
+                completed_at: '',
+                updated_at: ''
+            }
+
+            database.insert('tasks', task)
+
+            return res.writeHead(201).end()
+        }
+    }
+]
